8070deaf3a3a020f5d9c0911f342ff72d91c3981
PhilipSung <spinnavor@naver.com>
2023-05-03 17:29:05 +0900

    [ADD]MobX store : credit store file created

---
+++ src/store/creditStore.js
@@ -0,0 +1,14 @@
+import { makeObservable,
+         action,
+         observable,
+         autorun,
+         computed,
+        } from "mobx";
+
+class creditStore {
+    credits = 0;
+
+    constructor() {
+        
+    }
+}
